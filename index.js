9a0ce04fb57b488a1f7b8e65644d19f46f6d2e8b
Inêz Prazeres <inezprazeres@hotmail.com>
2023-03-12 14:56:18 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const number 9 = document.querySelector('.number-9')
+
+/*click no botao 9 
+comentario de varias linhas
+asdasdasd*/
+
+//comentario de uma linha
+
+number9.addEventListener('click',function(){
+    console.log('9')
+})
+
+console.log(number9)
